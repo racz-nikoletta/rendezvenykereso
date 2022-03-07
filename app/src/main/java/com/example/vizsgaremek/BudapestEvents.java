e609439e7bb1e24ea7382190fc12bdea9bda34c5
sz0juhade <sz0juhade@vasvari.org>
2022-03-07 09:29:04 +0100

    BudapestEvents osztály összekötése xml fájllal.

--- app/src/main/java/com/example/vizsgaremek/BudapestEvents.java
+++ app/src/main/java/com/example/vizsgaremek/BudapestEvents.java
@@ -1,4 +1,13 @@
 package com.example.vizsgaremek;
 
-public class BudapestEvents {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BudapestEvents extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_budapest_events);
+    }
 }
