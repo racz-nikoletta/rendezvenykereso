046044b844a8f64b343a8decea8a870829920c5f
sz0juhade <sz0juhade@vasvari.org>
2022-03-07 09:34:02 +0100

    SzegedEvents osztály és xml készítése, és a két fájl összekötése

---
+++ app/src/main/java/com/example/vizsgaremek/SzegedEvents.java
@@ -0,0 +1,13 @@
+package com.example.vizsgaremek;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SzegedEvents extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_szeged_events);
+    }
+}
