dc827bd819001e9a8350fd0cd260cbefa698fa99
sz0juhade <sz0juhade@vasvari.org>
2022-03-08 07:48:06 +0100

    DebrecenEvents java osztály létrehozása

---
+++ app/src/main/java/com/example/vizsgaremek/DebrecenEvents.java
@@ -0,0 +1,13 @@
+package com.example.vizsgaremek;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DebrecenEvents extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_debrecen_events);
+    }
+}
