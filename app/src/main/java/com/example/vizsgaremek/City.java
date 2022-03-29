8f9d816245584c58875d8836103b031690ca942e
sz0racnik <sz0racnik@vasvari.org>
2022-03-29 09:27:45 +0200

    Main activity elkezdve

--- app/src/main/java/com/example/vizsgaremek/City.java
+++ app/src/main/java/com/example/vizsgaremek/City.java
@@ -9,15 +9,18 @@ import androidx.appcompat.app.AppCompatActivity;
 public class City extends AppCompatActivity {
     Spinner citySpinner;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_city);
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_city);
 
         citySpinner = findViewById(R.id.citySpinner);
 
         String[] cities = new String[]{"Budapest", "Pécs", "Szeged", "Miskolc", "Debrecen"};
         final ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, cities);
         citySpinner.setAdapter(adapter);
+
+
+        }
     }
-}
+
