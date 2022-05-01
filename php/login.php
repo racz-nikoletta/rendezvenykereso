9f493f1f7088a08954cc4ace38bf1e4826cb8517
racz-nikoletta <77964770+racz-nikoletta@users.noreply.github.com>
2022-05-01 18:38:49 +0200

    Update login.php

--- php/login.php
+++ php/login.php
@@ -3,7 +3,7 @@ require "DataBase.php";
 $db = new DataBase();
 if (isset($_POST['email']) && isset($_POST['password'])) {
     if ($db->dbConnect()) {
-        if ($db->logIn("users", $_POST['email'], $_POST['password'])) {
+        if ($db->logIn("felhasznalok", $_POST['email'], $_POST['password'])) {
             echo "Login Success";
         } else echo "Username or Password wrong";
     } else echo "Error: Database connection";
