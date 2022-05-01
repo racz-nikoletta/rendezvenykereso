4e0fc0ff612e38406891f180274a0339f817b309
racz-nikoletta <77964770+racz-nikoletta@users.noreply.github.com>
2022-05-01 18:39:05 +0200

    Update signup.php

--- php/signup.php
+++ php/signup.php
@@ -3,7 +3,7 @@ require "DataBase.php";
 $db = new DataBase();
 if (isset($_POST['lastname']) && isset($_POST['firstname']) && isset($_POST['email']) && isset($_POST['password'])) {
     if ($db->dbConnect()) {
-        if ($db->signUp("users", $_POST['lastname'], $_POST['firstname'], $_POST['email'], $_POST['password'])) {
+        if ($db->signUp("felhasznalok", $_POST['lastname'], $_POST['firstname'], $_POST['email'], $_POST['password'])) {
             echo "Sign Up Success";
         } else echo "Sign up Failed";
     } else echo "Error: Database connection";
